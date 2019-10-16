4e227edd7b9b6c3c5471601950dd18c0f3d23ab6
wonwoo <aoruqjfu@gmail.com>
2019-10-16 21:43:29 +0900

    json file 로 변경

--- src/test/kotlin/ml/wonwoo/githubissuesdashboard/TestExtensions.kt
+++ src/test/kotlin/ml/wonwoo/githubissuesdashboard/TestExtensions.kt
@@ -1,5 +1,8 @@
 package ml.wonwoo.githubissuesdashboard
 
 import org.mockito.Mockito
+import org.springframework.core.io.ClassPathResource
 
 inline fun <reified T> any(): T = Mockito.any()
+
+fun ClassPathResource.readString(): String = inputStream.bufferedReader().readText()
